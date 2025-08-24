4d1dc98d3ee2410d8213b90243740b41bdd4ff07
Jason Wilkes <notarealdeveloper@gmail.com>
2025-08-24 18:18:15 -0500

    I figgad it out ramya

--- flake.nix
+++ flake.nix
@@ -19,17 +19,15 @@
       config.allowUnfree = true;
     };
 
-    lib = pkgs.lib;
-
     common   = (ps: with ps; [ ipython numpy is-instance python-bin ]);
     standard = (ps: with ps; [ pandas scikit-learn lightgbm lambda-multiprocessing ]);
 
     pythons = with pkgs; {
-      py314  = (python314.withPackages (ps: with ps; common ++ standard));
-      py315  = (python315.withPackages (ps: with ps; common ++ standard));
-      py313t = (python313FreeThreading.withPackages (ps: with ps; common));
-      py314t = (python314FreeThreading.withPackages (ps: with ps; common));
-      py315t = (python315FreeThreading.withPackages (ps: with ps; common));
+      py314  = python314.withPackages (ps: with ps; common ++ standard);
+      py315  = python315.withPackages (ps: with ps; common ++ standard);
+      py313t = python313FreeThreading.withPackages (ps: with ps; common);
+      py314t = python314FreeThreading.withPackages (ps: with ps; common);
+      py315t = python315FreeThreading.withPackages (ps: with ps; common);
     };
 
     default = with pkgs; buildEnv {
@@ -38,20 +36,19 @@
       paths = builtins.attrValues pythons;
     };
 
-    attrset  = import ./src {} pkgs;
-    packages = lib.filterAttrs (k: v: lib.isDerivation v) attrset;
+    packages = (import ./src {} pkgs) // pythons // { inherit default; };
 
   in
 
   {
 
-    packages.${system} = pythons;
+    packages.${system} = packages;
 
     checks.${system} = {
 
       py315 = pkgs.runCommand "py315" { } ''
           set -euo pipefail
-          ${packages.py314}/bin/python - << 'EOF'
+          ${packages.py315}/bin/python - << 'EOF'
           import sklearn
           import lightgbm
           EOF
