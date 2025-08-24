9df34743291c41d69ae37f3fc895c47fa337b475
Jason Wilkes <notarealdeveloper@gmail.com>
2025-08-24 17:55:46 -0500

    Who knows if it'll help or hurt, but I'm liking it

--- flake.nix
+++ flake.nix
@@ -19,45 +19,43 @@
       config.allowUnfree = true;
     };
 
-    default = with pkgs; buildEnv {
+    common   = (ps: with ps; [ ipython numpy is-instance python-bin ]);
+    standard = (ps: with ps; [ pandas scikit-learn lightgbm lambda-multiprocessing ])
 
+    pythons = {
+      py314  = python314.withPackages (ps: with ps; common ++ standard);
+      py315  = python315.withPackages (ps: with ps; common ++ standard);
+      py313t = python313FreeThreading.withPackages (ps: with ps; common);
+      py314t = python314FreeThreading.withPackages (ps: with ps; common);
+      py315t = python315FreeThreading.withPackages (ps: with ps; common);
+    }
+
+    default = with pkgs; buildEnv {
       name = "overlay";
       ignoreCollisions = true;
-
-      paths = [
-
-        (python314.withPackages (ps: with ps; [
-          numpy
-          pandas
-          python-bin
-        ]))
-
-        (python315.withPackages (ps: with ps; [
-          numpy
-          pandas
-          python-bin
-        ]))
-
-        (python314FreeThreading.withPackages (ps: with ps; [
-          numpy
-          python-bin
-        ]))
-
-        (python315FreeThreading.withPackages (ps: with ps; [
-          numpy
-          python-bin
-        ]))
-
-      ];
+      paths = builtins.attrValues pythons;
     };
+
+    packages = (import ./src {} pkgs) // pythons // { inherit default; };
 
   in
 
   {
 
-    packages.${system} = (import ./src {} pkgs) // { inherit default; };
+    packages.${system} = packages;
+
+    checks.${system} = {
+
+      py315 = pkgs.runCommand "py315" { } ''
+          set -euo pipefail
+          ${packages.py315}/bin/python - << 'EOF'
+          import sklearn
+          import lightgbm
+          EOF
+          touch $out
+        '';
+    };
 
     overlays.default = overlay;
-
   };
 }
