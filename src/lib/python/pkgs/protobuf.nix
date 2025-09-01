decd0868ba074dd97a7165959bf460b1eaae7cf7
Jason Wilkes <notarealdeveloper@gmail.com>
2025-09-01 12:00:24 -0500

    Fix syntax in pwotobuf uwu

--- src/lib/python/pkgs/protobuf.nix
+++ src/lib/python/pkgs/protobuf.nix
@@ -4,13 +4,13 @@
 , python
 }:
 
-  protobuf = buildPythonPackage rec {
-    pname = "protobuf";
-    version = "6.31.1";
-    pyproject = true;
-    build-system = with python.pkgs; [ setuptools ];
-    src = fetchPypi {
-      inherit pname version;
-      sha256 = "sha256-2MrEyYLwuVek3HOoDi6iT6sI5nnA3p3rg19KEtaaypo=";
-    };
+buildPythonPackage rec {
+  pname = "protobuf";
+  version = "6.31.1";
+  pyproject = true;
+  build-system = with python.pkgs; [ setuptools ];
+  src = fetchPypi {
+    inherit pname version;
+    sha256 = "sha256-2MrEyYLwuVek3HOoDi6iT6sI5nnA3p3rg19KEtaaypo=";
   };
+}
