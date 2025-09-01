04d71db489ec95af818b2cdbb6e7c159a2325aa5
Jason Wilkes <notarealdeveloper@gmail.com>
2025-09-01 09:35:05 -0500

    Add autoPatchelfHook to src/lib/python/pkgs/tensorflow-313.nix

--- src/lib/python/pkgs/tensorflow-313.nix
+++ src/lib/python/pkgs/tensorflow-313.nix
@@ -1,26 +1,33 @@
-{ lib, fetchurl, buildPythonPackage, python }:
+{ lib
+, fetchurl
+, buildPythonPackage
+, python
+, stdenv
+, autoPatchelfHook
+}:
 
-# Pure wheel install; no dependency on pyprev.tensorflow-bin
-buildPythonPackage {
-  pname   = "tensorflow";
-  version = "2.20.0";
-  format  = "wheel";
+  buildPythonPackage {
+    pname   = "tensorflow";
+    version = "2.20.0";
+    format  = "wheel";
 
-  src = fetchurl {
-    url = "https://files.pythonhosted.org/packages/43/fb/8be8547c128613d82a2b006004026d86ed0bd672e913029a98153af4ffab/tensorflow-2.20.0-cp313-cp313-manylinux_2_17_x86_64.manylinux2014_x86_64.whl";
-    # TODO: replace with real sha256 once you fetch it
-    sha256 = lib.fakeSha256;
-  };
+    src = fetchurl {
+      url = "https://files.pythonhosted.org/packages/43/fb/8be8547c128613d82a2b006004026d86ed0bd672e913029a98153af4ffab/tensorflow-2.20.0-cp313-cp313-manylinux_2_17_x86_64.manylinux2014_x86_64.whl";
+      sha256 = "sha256-X6NymwEm91qZiCuJ+31TZRVyHtqAFKY+JZ54C6Cjc3I=";
+    };
 
-  # keep it minimal; add deps later if import complains
-  propagatedBuildInputs = [ ];
+    nativeBuildInputs = [ autoPatchelfHook ];
 
-  pythonImportsCheck = [ "tensorflow" ];
+    buildInputs = [ stdenv.cc.cc ];
 
-  meta = with lib; {
-    description = "TensorFlow wheel (pinned) for CPython 3.13";
-    homepage    = "https://www.tensorflow.org/";
-    license     = licenses.asl20;
-    platforms   = platforms.linux;
-  };
-}
+    propagatedBuildInputs = [ ];
+
+    pythonImportsCheck = [ "tensorflow" ];
+
+    meta = with lib; {
+      description = "TensorFlow wheel Python 3.13";
+      homepage    = "https://www.tensorflow.org";
+      license     = licenses.asl20;
+      platforms   = platforms.linux;
+    };
+  }
