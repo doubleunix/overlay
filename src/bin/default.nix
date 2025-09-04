19b8f761dfa8515416d514824bf4927b76f5b31a
Jason Wilkes <notarealdeveloper@gmail.com>
2025-09-04 11:27:14 -0500

    Add kdenlive override

--- src/bin/default.nix
+++ src/bin/default.nix
@@ -6,4 +6,13 @@ final: prev:
   noelf = prev.callPackage ./noelf.nix { };
 
   weatherspect = prev.callPackage ./weatherspect.nix { };
+
+  kdenlive = (prev.kdenlive.overrideAttrs (prev: {
+    nativeBuildInputs = (prev.nativeBuildInputs or []) ++ [ makeBinaryWrapper ];
+    postInstall = (prev.postInstall or "") + ''
+      wrapProgram $out/bin/kdenlive \
+        --prefix LADSPA_PATH : ${prev.rnnoise-plugin}/lib/ladspa
+    '';
+  }));
+
 }
