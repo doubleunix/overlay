11e994c66da38a57c7adaa860f85af0e498015e2
Jason Wilkes <notarealdeveloper@gmail.com>
2025-09-04 11:27:59 -0500

    Ugh

--- src/bin/default.nix
+++ src/bin/default.nix
@@ -8,7 +8,7 @@ final: prev:
   weatherspect = prev.callPackage ./weatherspect.nix { };
 
   kdenlive = (prev.kdenlive.overrideAttrs (prev: {
-    nativeBuildInputs = (prev.nativeBuildInputs or []) ++ [ makeBinaryWrapper ];
+    nativeBuildInputs = (prev.nativeBuildInputs or []) ++ [ prev.makeBinaryWrapper ];
     postInstall = (prev.postInstall or "") + ''
       wrapProgram $out/bin/kdenlive \
         --prefix LADSPA_PATH : ${prev.rnnoise-plugin}/lib/ladspa
