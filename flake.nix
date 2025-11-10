690b8dba15b675f27d569637f02b636daacf846a
Jason Wilkes <notarealdeveloper@gmail.com>
2025-11-10 09:42:35 -0600

    Syntax error

--- flake.nix
+++ flake.nix
@@ -102,7 +102,7 @@
     pythons = with pkgs; {
       py311  = python311.withPackages (ps: [
         pip
-      ])
+      ]);
       py313  = python313.withPackages (ps: all ps ++ std ps ++ py313 ps);
       py314  = python314.withPackages (ps: all ps ++ std ps ++ py314 ps);
       py315  = python315.withPackages (ps: all ps ++ std ps ++ py315 ps);
