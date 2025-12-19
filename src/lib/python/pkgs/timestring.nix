4e9f03915d5796e89fda7a854c29d34abc558da3
Jason Wilkes <root@thedynamiclinker.com>
2025-12-19 11:10:19 -0600

    Fix dis plz

--- src/lib/python/pkgs/timestring.nix
+++ src/lib/python/pkgs/timestring.nix
@@ -1,31 +1,26 @@
-{ lib
+{
+  lib
 , buildPythonPackage
-, fetchPypi
+, fetchFromGitHub
 , setuptools
-, python
-, ... }:
+, pytz
+, ...
+} @ inputs:
 
 buildPythonPackage rec {
   pname = "timestring";
   version = "1.6.4";
 
-  format = "setuptools";
-
-  src = fetchPypi {
-    inherit pname version;
-    sha256 = "b7d1a2060a5f2e34d0c93b1042690074afcea66638fa2cf4dcad2d1bec4deacc";
+  src = fetchFromGitHub {
+    owner = "codecov";
+    repo = "timestring";
+    tag = version;
+    hash = "";
   };
 
-  nativeBuildInputs = [ setuptools ];
+  build-system = [ setuptools ];
 
-  propagatedBuildInputs = [ ];
-
-  # Optional metadata
-  meta = with lib; {
-    description = "Converts human-expressed time strings to Date/Range objects";
-    homepage = "https://pypi.org/project/timestring/";
-    license = licenses.asl20;
-    maintainers = with maintainers; [ ];
-  };
+  propagatedBuildInputs = [
+    pytz
+  ];
 }
-
